7a14e9a87fd107794e8249c559c75dfef015eaef
solo <solomon.munyalo@student.moringaschool.com>
2021-06-17 13:27:52 +0300

    define days of the week and months oth the year

--- scripts.js
+++ scripts.js
@@ -2,4 +2,10 @@
 const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", " Yaw", "Kofi", "Kwame"];
 
 //female akan names according to the day of the week
-const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];  
+const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"] //Days of the week
+const noOfDays = 7 //number of days in a week
+
+//total Days Of Months in a year
+const monthsOfTheYear = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+const totalDaysOfMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
