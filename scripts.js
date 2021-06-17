2adedc316dbd8ea8174cf1e48c6f167bb574cb41
solo <solomon.munyalo@student.moringaschool.com>
2021-06-17 13:39:25 +0300

    add month code generator

--- scripts.js
+++ scripts.js
@@ -9,3 +9,15 @@ const noOfDays = 7 //number of days in a week
 //total Days Of Months in a year
 const monthsOfTheYear = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 const totalDaysOfMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+let monthCode = [] //month code according to Gregian calendar system
+
+let monthCodeGenerator = () => {
+  let mod = 0;
+  for (let i = 0; i < totalDaysOfMonths.length; i++) {
+    monthCode.push(mod);
+    mod = (mod + totalDaysOfMonths[i]) % noOfDays
+
+  }
+  return mod;
+} //month Code Generator
