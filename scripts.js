7b3c03bd76bdddb0a7c7873507c37e5d3bbfa450
solo <solomon.munyalo@student.moringaschool.com>
2021-06-17 13:17:02 +0300

    define female and male names

---
+++ scripts.js
@@ -0,0 +1,5 @@
+//male akan names according to the day of the week
+const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", " Yaw", "Kofi", "Kwame"];
+
+//female akan names according to the day of the week
+const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
